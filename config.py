551c4276d4953cdb95dd5dc276d5d3d316b73f78
qw1zzard <qw1gor.p@gmail.com>
2022-09-16 15:56:07 +0300

    Refactore code

--- config.py
+++ config.py
@@ -4,19 +4,15 @@ from databases import Database
 import os
 
 
-
-
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
 
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
